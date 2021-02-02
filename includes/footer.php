5f9bdc456b956e8d9bf586be5c20adfb6d67bb61
Loke <christian.bresson@hotmail.com>
2021-02-02 14:15:48 +0100

    Git Init

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<div class="footer">
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+
+
+</div>
